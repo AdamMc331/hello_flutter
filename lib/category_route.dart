920b10ec4133c545923fc1f6b035eff5f4dad066
AdamMc331 <amcneilly331@gmail.com>
2018-06-10 15:59:57 -0400

    Built ListView for Lesson 1.15.

---
+++ lib/category_route.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'category.dart';
+
+final _backgroundColor = Colors.green[100];
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  Widget _buildCategoryWidget(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.cake,
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.all(8.0),
+      child: _buildCategoryWidget(categories),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'category.dart';
-
-const _categoryName = 'Cake';
-const _categoryColor = Colors.green;
-const _categoryIcon = Icons.cake;
+import 'category_route.dart';
 
 void main() {
   runApp(UnitConverterApp());
@@ -17,13 +13,7 @@ class UnitConverterApp extends StatelessWidget {
       title: 'Unit Converter',
       home: Scaffold(
         backgroundColor: Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
+        body: CategoryRoute(),
       ),
     );
   }
